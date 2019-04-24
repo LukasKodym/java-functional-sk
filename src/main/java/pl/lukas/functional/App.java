164b307cd02c01acddf030f03c56d3bd257eef6d
LukasKodym <lukasz.kodym@gmail.com>
2019-04-24 17:59:11 +0200

    lambda expressions, end 4.3

--- src/main/java/pl/lukas/functional/App.java
+++ src/main/java/pl/lukas/functional/App.java
@@ -4,24 +4,15 @@ public class App {
 
     public static void main(String[] args) {
 
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("A");
-            }
-        };
-
         Runnable ru = () -> System.out.println("A");
+        Runnable run = () -> System.out.println("B");
 
-        Comparable<String> c = new Comparable<String>() {
-            @Override
-            public int compareTo(String o) {
-                return 0;
-            }
-        };
+        test("A", ru);
+        test("B", run);
 
-        Comparable<String> co =  o -> 0;
+    }
 
-        Movable m = direction -> 11;
+    public static void test(String name, Runnable a) {
+
     }
 }
